e43b47c551b66afe74c25016c62eec13002e738e
Aryan Singh Thakur <aryana123b456@gmail.com>
2024-09-29 12:13:25 +0530

    Store location check

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import store from "./store/store.js";
+import store from "./store/Store.js";
 import {
   Route,
   RouterProvider,
